35569b98a4e205e392e189aec59686aff9a755e5
belgeu <belgeu777@gmail.com>
2017-02-12 11:30:02 -0500

    Initial revision of webdriver test for Ruby lang

---
+++ webdriver_test.rb
@@ -0,0 +1,33 @@
+"""
+Webdriver test (CentOS7)
+
+To run test:
+- install ruby
+    yum install ruby ruby-devel
+- install gcc
+    yum install gcc
+- install latest Selenium version
+    gem install selenium-webdriver
+- install latest Firefox browser
+    yum install firefox
+- get latest geckodriver
+    https://github.com/mozilla/geckodriver/releases
+
+Execute:
+# ruby webdriver_test.rb
+
+
+GIT
+
+mkdir /root/python/practice
+cd /root/python/practice
+git clone https://github.com/belgeu/misc.git
+cd misc
+## edit .git/config to ask pass (add belgeu@ to url)
+## url = https://belgeu@github.com/belgeu/misc.git
+git add webdriver_test.rb
+git commit -m "Initial revision of webdriver test for Ruby"
+git push origin master
+
+"""
+
